fa43bb7145ecb179aec1ad6e9c216b950541f116
Franccesco Jaimes <franccescojaimesagreda@gmail.com>
2026-02-27 03:44:01 -0500

    feat: add first steps

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'SEO Title',
+  description: 'SEO Description',
+  keywords: ['Contact Page', 'Franccesco', 'información', '...'],
+};
+
+export default function ContactPage() {
+  return (
+    <span className="text-7xl">Contact Page</span>
+  )
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'SEO Title',
+  description: 'SEO Description',
+  keywords: ['Pricing Page', 'Franccesco', 'información', '...'],
+};
+
+export default function PricingPage() {
+  return (
+    <span className="text-7xl">Pricing Page</span>
+  )
+}
